d33b1e78044df5ab30ca8d86dc464fd922ebb45d
Hill-Boss <bosscodycollege@yahoo.com>
2020-01-27 14:44:33 -0700

    HW2

--- HW2/js/main.js
+++ HW2/js/main.js
@@ -7,3 +7,18 @@ Purpose: JavaScript
 **/
 
 console.log("JavaScript Rules!");
+console.log("Favorite Websites");
+console.log("Twitch");
+console.log("YouTube");
+console.log("Spotify");
+console.log("");
+console.log("Favorite Games");
+console.log("ARK: Survival Evolved");
+console.log("Rocket League");
+console.log("Minecraft (modded)");
+console.log("");
+console.log("Favorite artists");
+console.log("Barns Courtney");
+console.log("Max Frost");
+console.log("Mumford & Sons");
+console.log("");
