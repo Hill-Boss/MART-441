0b06355e0e99e50df0a7369778d133f429d5095f
Hill-Boss <bosscodycollege@yahoo.com>
2020-01-29 23:31:18 -0700

    HW3 idea and init

---
+++ HW3/js/main.js
@@ -0,0 +1,20 @@
+// The story will start the first choice will  be to run, hide, fight or other
+// more story happend the choice2
+//
+//
+//
+
+function choice1(choice) {
+    if (choice == 'run') {
+
+    }
+    else if (choice == 'hide') {
+
+    }
+    else if (choice == 'fight') {
+
+    }
+    else {
+        random('choice1');
+    }
+}
