acd88187814fae61bc2b1e6832574694626626b1
Hill-Boss <bosscodycollege@yahoo.com>
2020-02-08 17:57:43 -0700

    HW4
    user input for story

---
+++ HW4/js/main.js
@@ -0,0 +1,113 @@
+// The story will start the first choice will  be to run, hide, fight or other
+// more story happend the choice2
+//
+//
+//
+
+results = {'1G':'You stick with your fellow soldiers and fight alongside them, the battle is long but your side is victorious.',
+           '1M':'You run ahead straight at the enemy, somehow you manage to fight and stay alive long enough for some of your fellow soldiers to back you up.',
+           '1B':'You hide in some shrubs and your commanding officer orders someone to execute you for being a coward.',
+           '2G':'You manage to hide behind a shield and survive the volley.',
+           '2M':'You charge forward as the arrows fall behind you.',
+           '2B':'You try to cut the arrows as the fly toward you, you do not succeed.',
+           '3G':'You run in the opossite direction and manage to make it to a nearby forest, you aviod capture.',
+           '3M':'You hide in the sewer and the gaurds cannot find you.',
+           '3B':'You try to fight the gaurds head on, only to be quickly captured. The other men with you all escape.',
+           '4G':'',
+           '4M':'',
+           '4B':'',
+           '5G':'',
+           '5M':'',
+           '5B':'',
+
+}
+
+next = ['Once upon a time, there lived a King who only knew war. '+
+        'For as long as he reigned, his nation was at war with at least three others. '+
+        'You have been conscripted into his royal army. '+
+        'You have been marching for months towards your first battle. '+
+        'Once the battle starts, ', // next[0] is not used yet
+        '<br>'+
+        'After your first experience with war, you find yourself still alive even if only barely. '+
+        'Next, you will siege a castle. '+
+        'Once the battle begins you are barraged with arrows. ',
+        '<br>'+
+        'The siege fails, you and a few dozen soldiers are captured. '+
+        'One of the other men makes a run for it giving you only seconds to decide what to do. ',
+        '<br>'+
+        ''+
+        '',
+        '<br>'+
+        ''+
+        '',
+]
+
+
+Uchoice = [-1,-1,-1,-1,-1]; // this array holds the users choices
+Gchoice = [0,2,1,-1,-1,-1]; // this array is the good choice for each stage
+Bchoice = [2,0,0,-1,-1,-1]; // this array is the bad choice for each stage
+
+i = 0;
+
+function rand_choice() {
+  return Math.floor(Math.random() * 3);
+}
+
+function choice(choice) {
+    if (i <= 2) {
+        if (choice == 'fight' || choice == 'Fight') {
+            Uchoice[i] = 0;
+        } else if (choice == 'run' || choice == 'Run') {
+            Uchoice[i] = 1;
+        } else if (choice == 'hide' || choice == 'Hide') {
+            Uchoice[i] = 2;
+        } else {
+            Uchoice[i] = rand_choice();
+        }
+        if (Uchoice[i] == Gchoice[i]) {
+            update_story(true);
+        } else if (Uchoice[i] == Bchoice[i]) {
+            update_story(false);
+        } else {
+            update_story('meh');
+        }
+        i++;
+    }
+}
+
+function update_story(qual) {
+  story = document.getElementById('Story');
+  if (qual == true) { // good choice
+      story.innerHTML += results[''.concat((i+1), 'G')];
+
+  } else if (qual == false) { // bad choice
+      story.innerHTML += results[''.concat((i+1), 'B')];
+  } else { // meh choice
+      story.innerHTML += results[''.concat((i+1), 'M')];
+  }
+  if (qual != false && i <= 1) {
+      story.innerHTML += next[i+1];
+  } else {
+      end_story();
+  }
+}
+
+function get_input() {
+  Uchoice[i] = choice(document.getElementById('input').value)
+}
+
+function Restart() {
+  document.getElementById('Story').innerHTML = next[0];
+  document.getElementById('choices').style.display = 'block';
+  document.getElementById('restart').style.display = 'none';
+  i = 0;
+}
+
+function end_story() {
+    story = document.getElementById('Story');
+    story.innerHTML += '<h3>The End!</h3>';
+    document.getElementById('choices').style.display = 'none';
+    document.getElementById('restart').style.display = 'block';
+
+
+}
---
+++ HW4/readme.md
@@ -0,0 +1,29 @@
+You are to create a short “choose your own adventure story” on a
+single page. You are going to use JavaScript to make choices, access
+the DOM, and print something out. It should have a title, the story,
+and at least the three progressions in the story. Please add images,
+colors, fonts, etc. via CSS to set the scene.
+Specifics:
+1. Use if statements
+2. Use variables
+3. Use function(s)
+4. Use addition or concatenation
+5. Have at least three choices in your story
+6. Include images, colors, and fonts to your story via external CSS
+sheet
+7. Each choice should update something in the DOM
+Please save your work and upload it to your repository. You will
+continue working on this next week.
+
+Please include a readme that discusses your successes, challenges, and
+how you overcame them.
+
+Successes:
+used onclick to run a js function
+updating the story.
+ending the story on bad choice or reaching the end.
+
+
+Challenges:
+being creative with a story.
+setting up logic for choices.
