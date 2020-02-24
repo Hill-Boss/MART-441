7767319ac17e18a213e8614d58c5af54715cb6e2
Hill-Boss <bosscodycollege@yahoo.com>
2020-02-24 06:35:42 -0700

    HW6

---
+++ HW6/index.js
@@ -0,0 +1,30 @@
+var fields = {
+    'firstname': '',
+    'lastname': '',
+    'age': '',
+    'attempts': 0
+}
+
+function getInfo() {
+    document.getElementById('info').innerHTML =
+        "<label for=\"firstname\"></label>" +
+        "<input class=\"w3-input\" type=\"text\" name=\"firstname\" placeholder=\"First name\"><br>" +
+        "<label for=\"lastname\"></label>" +
+        "<input class=\"w3-input\" type=\"text\" name=\"lastname\" placeholder=\"Last name\"><br>" +
+        "<label for=\"age\"></label>" +
+        "<input class=\"w3-input\" type=\"text\" name=\"age\" placeholder=\"Age\"><br>" +
+        "<div class=\"w3-btn w3-green\" onclick=\"gotoPage(\'Game\');\">Start Game</div>";
+}
+
+function setInfo() {
+    fields['firstname'] = document.getElementByName('firstname').value;
+    fields['lastname'] = document.getElementByName('lastname').value;
+    fields['age'] = document.getElementByName('age').value;
+
+    localStorage.setItem('information', JSON.stringify(fields));
+}
+
+function gotoPage(page) {
+    setInfo()
+    window.location = page;
+}
